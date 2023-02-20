3776f89b4338546894b931a30ec75e094ad46de8
duyen <ngduyen2605@gmail.com>
2023-02-20 22:56:19 +0700

    set up project structure and create employee component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import Employee from './Pages/Employee'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Routes>
+      <Route path='/' element={<Employee />} />
+      {/* <Route path='/orders' element={<Orders />} />
+      <Route path='/products' element={<Products />} />
+      <Route path='/customers' element={<Customers />} /> */}
+    </Routes>
+  )
 }
-
-export default App;
